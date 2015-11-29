38a30924aab3cdae2e240dee76b358fd229f54d3
jeffreymeng <jkmexpress@yahoo.com>
2015-11-29 11:08:57 -0800

    edit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function get(child) {
+	var fb = new Firebase("https://synctimer.firebaseio.com")
+	fb.child(child).once("value", function(data) {
+  		
+	});
+}
+function getTime(date) {
+    var d = new Date(date);
+    return {
+    	"seconds": d.getSeconds(),
+    	"minutes": d.getMinutes(),
+    	"hours": d.getHours(),
+    	"day": d.getDay(),
+    	"date": d.getDate(),
+    	"month": d.getMonth()
+    }
+
+}
