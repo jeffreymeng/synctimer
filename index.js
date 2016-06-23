b91dc4db87cc98c3130c1db76c7f9cedf4ce89df
jeffmeng <jkmexpress@yahoo.com>
2016-06-23 21:10:25 +0000

    bug fid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function get(child) {
 	var fb = new Firebase("https://synctimer.firebaseio.com")
 	fb.child(child).once("value", function(data) {
-  		
+  		var timerdata = data.val();
 	});
 }
 function getTime(date) {
